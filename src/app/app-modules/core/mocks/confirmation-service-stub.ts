d38bb79ea5dc296da046e2e1e4eb07c603bd40a6
Karyamsetty Helen Grace <KA40094929@wipro.com>
2024-01-09 19:24:44 +0530

    initial commit

---
+++ src/app/app-modules/core/mocks/confirmation-service-stub.ts
@@ -0,0 +1,44 @@
+/* 
+* AMRIT – Accessible Medical Records via Integrated Technology 
+* Integrated EHR (Electronic Health Records) Solution 
+*
+* Copyright (C) "Piramal Swasthya Management and Research Institute" 
+*
+* This file is part of AMRIT.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see https://www.gnu.org/licenses/.
+*/
+
+
+export class ConfirmationServiceStub {
+
+  public confirm(title: string, message: string, btnOkText: string = 'OK', btnCancelText: string = 'Cancel') {
+  }
+
+  public alert(message: string, status: string = 'info', btnOkText: string = 'OK'): void {
+  }
+
+  public remarks(message: string, titleAlign: string = 'center', messageAlign: string = 'center', btnOkText: string = 'Submit', btnCancelText: string = "Cancel") {
+  }
+
+  public editRemarks(message: string, comments: string, titleAlign: string = 'center', messageAlign: string = 'center', btnOkText: string = 'Submit') {
+  }
+
+  public notify(message: string, mandatories: any, titleAlign: string = 'center', messageAlign: string = 'center', btnOkText: string = 'OK') {
+  }
+
+  public choice(message: string, values: any, titleAlign: string = 'center', messageAlign: string = 'center', btnOkText: string = 'Confirm', btnCancelText: string = 'Cancel') {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,17 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialModule } from './app-modules/core/material.module';
+import { UserLoginModule } from './user-login/user-login.module';
+import { CoreModule } from './app-modules/core/core.module';
+import { HTTP_INTERCEPTORS, HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpInterceptorService } from './app-modules/core/services/http-interceptor.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -10,9 +19,26 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    MatIconModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    UserLoginModule,
+    CoreModule.forRoot()
   ],
-  providers: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],  
+  providers: [
+    HttpClient,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpInterceptorService,
+      multi: true,
+    },
+  ],  
   bootstrap: [AppComponent]
 })
 export class AppModule { }
